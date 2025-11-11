71fc15f723d99937cf1729cfd462d280043608b1
cmoulaman-ux <c.moulaman@gmail.com>
2025-11-11 15:38:34 +0100

    FILTRE ADMIN

--- app.py
+++ app.py
@@ -570,7 +570,7 @@ def index():
           <img src="/static/img/assos/gravity2.png" alt="Gravity">
         </a>
         <a href="https://www.facebook.com/groups/1947945858558429" target="_blank" title="Moto Liberté sur Facebook">
-         <img src="/static/img/assos/motoliberte.jpg" alt="Moto Liberté" style="height:80px; margin:8px; border-radius:8px;">
+         <img src="/static/img/assos/motoliberte2.png" alt="Moto Liberté" style="height:80px; margin:8px; border-radius:8px;">
         </a>
       </div>
       <p class="muted center" style="margin-top:8px;">
