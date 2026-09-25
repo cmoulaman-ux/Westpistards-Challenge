aa81f562b61f24c3c591977a5a54ef1be9acba8a
cmoulaman-ux <c.moulaman@gmail.com>
2026-09-25 15:17:53 +0200

    logo wp change 1

--- app.py
+++ app.py
@@ -604,7 +604,7 @@ def index():
     <section class="bg-hero">
       <div class="bg-hero-yt">
         <iframe
-          src="https://www.youtube.com/embed/videoseries?list=PLPm1klG-O5duFLlins-RZ4E1g3NOYH9qU&autoplay=1&mute=1&controls=0&rel=0&loop=1&playlist=PLPm1klG-O5duFLlins-RZ4E1g3NOYH9qU&modestbranding=1&playsinline=1&enablejsapi=1&origin=https://westpistards-challenge.onrender.com"
+          src="https://www.youtube.com/embed/sBA9yxX9fLg?autoplay=1&mute=1&controls=0&rel=0&loop=1&playlist=sBA9yxX9fLg&modestbranding=1&playsinline=1&enablejsapi=1&origin=https://westpistards-challenge.onrender.com"
           title="Background video"
           allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
           frameborder="0">
