134ebe1e2f7c5aa9056a04728693f0b2d11e4d61
cmoulaman-ux <c.moulaman@gmail.com>
2026-09-25 16:21:33 +0200

    add link honda moto

--- app.py
+++ app.py
@@ -547,7 +547,7 @@ def index():
         <a class="partner" href="https://www.facebook.com/share/17Jn1CirtS" target="_blank" rel="noopener" title="ECF">
           <img src="/static/img/partners/ecf.jpg" alt="ECF">
         </a>
-	<a class="partner" href="https://www.facebook.com/search/top?q=honda%20moto" target="_blank" rel="noopener" title="Honda Moto">
+	<a class="partner" href="https://www.facebook.com/HondaMotoFrance" target="_blank" rel="noopener" title="Honda Moto">
           <img src="/static/img/partners/honda.jpg" alt="Honda">
         </a>
       </div>
